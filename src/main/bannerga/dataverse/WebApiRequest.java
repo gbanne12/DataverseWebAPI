fec325cfc15cf3632b808f503412cfaa53723a08
gbannermanIncremental <gary.bannerman@incrementalgroup.co.uk>
2023-01-27 16:38:47 +0000

    Use the MSAL4J implementation for obtaining the access token.
    (MSAL4J is the replacement for ADAL4J which was previously used to get the token)

--- src/main/bannerga/dataverse/WebApiRequest.java
+++ src/main/bannerga/dataverse/WebApiRequest.java
@@ -1,38 +1,38 @@
 package bannerga.dataverse;
 
 import bannerga.config.ConfigBuilder;
-import com.microsoft.aad.adal4j.AuthenticationContext;
-import com.microsoft.aad.adal4j.AuthenticationResult;
-import com.microsoft.aad.adal4j.ClientCredential;
+import com.microsoft.aad.msal4j.*;
 import net.minidev.json.JSONObject;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Collections;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public interface WebApiRequest {
 
-    JSONObject send(String uri, String body) throws IOException;
+    JSONObject send(String uri, String body) throws IOException, ExecutionException, InterruptedException;
 
-    default String getToken() {
+    default String getToken() throws MalformedURLException {
         var config = ConfigBuilder.build();
-        String token;
-        ExecutorService service = null;
-        try {
-            service = Executors.newFixedThreadPool(1);
-            var authContext = new AuthenticationContext("https://login.microsoftonline.com/" + config.tenantId(), false, service);
-            var credential = new ClientCredential(config.clientId(), config.clientSecret());
-            AuthenticationResult result = authContext.acquireToken(config.resourceUrl(), credential, null).get();
-            token = result.getAccessToken();
-        } catch (ExecutionException | InterruptedException | MalformedURLException e) {
-            throw new RuntimeException("Failed to get obtain a token. Cannot use the WebApi: " + e);
-        } finally {
-            assert service != null;
-            service.shutdown();
-        }
-        return token;
+
+        // This is the secret that is created in the Azure portal when registering the application
+        IClientCredential clientSecret = ClientCredentialFactory.createFromSecret(config.clientSecret());
+        ConfidentialClientApplication app =
+                ConfidentialClientApplication
+                        .builder(config.clientId(), clientSecret)
+                        .authority("https://login.microsoftonline.com/" + config.tenantId())
+                        .build();
+
+        Set<String> defaultScope = Collections.singleton(config.resourceUrl() + "/.default");
+        ClientCredentialParameters parameters =
+                ClientCredentialParameters
+                        .builder(defaultScope)
+                        .build();
+
+        IAuthenticationResult result = app.acquireToken(parameters).join();
+        return result.accessToken();
     }
 
 }
